1ede8a3fb6fd4f2c43f4df5c4b38e650af60cfeb
git-of-neo <nshmail55@gmail.com>
2024-01-05 11:30:20 +0800

    add: write header

--- utils/scrapper.py
+++ utils/scrapper.py
@@ -8,6 +8,7 @@ r = requests.get(url)
 tree = r.json()["tree"]
 with open("files.csv", "w", newline="") as f:
     writer = csv.writer(f)    
+    writer.writerow(("rowid", "filepath"))
     for idx, file in enumerate(tree):
         writer.writerow((idx, file["path"]))
 
